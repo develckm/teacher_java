d4ec7e9cdf9aa7cd005e5d4e450e06e5d86be7e3
develckm <develckm@gmail.com>
2022-03-30 17:32:15 +0900

    ecripse test

--- src/com/econ/L18Switch.java
+++ src/com/econ/L18Switch.java
@@ -59,10 +59,27 @@ public class L18Switch {
 		System.out.println("당신의 성적은 "+grade);
 		System.out.println("성적 입력 프로그램입니다.");
 		System.out.println("점수 입력:");
+		////////////
 		Scanner sc=new Scanner(System.in);
 		String str_score=sc.nextLine();
 		int score2=Integer.parseInt(str_score);
 		GradeApp gradeApp=new GradeApp(score2);
 		System.out.println("당신의 성적은 "+gradeApp.getGrade()+" 입니다.");
+		////////////
+		
+//		 gradeApp=new GradeApp(77);
+//		 gradeApp=new GradeApp(99);
+//		 gradeApp=new GradeApp(59); 
+//		gradeApp.setScore(77);
+//		gradeApp.setScore(99);
+//		gradeApp.setScore(59);
+
+		//GradeApp.setScore()를 구현해서 사용하세요! (생성자와 거의 비슷하다) 
+		int[] score_arr=new int[7]; //7번 성적을 입력 받으 후 grade_arr 를 완성하세고 grade_arr 를 출력하세요!
+		String[] grade_arr=new String[7];
 	}
 }
+
+
+
+
