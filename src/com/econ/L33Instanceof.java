d98a7efaec176fd392ac9e5fd9eb5fce19566555
develckm <develckm@gmail.com>
2022-04-04 16:56:49 +0900

    업캐스팅 예제

--- src/com/econ/L33Instanceof.java
+++ src/com/econ/L33Instanceof.java
@@ -6,9 +6,21 @@ class Child extends Parent implements Personable{
 class Parent{
 	int a=10;
 }
-
+class ArraryPrint{
+	ArraryPrint(Object[]nums){
+		String msg="";
+		for(int i=0; i<nums.length; i++) {
+			msg+=nums[i].toString()+",";
+		}
+		System.out.println(msg);
+	}
+}
 public class L33Instanceof {
 	public static void main(String []args) {
+		Object[]nums= {10,20,30};
+		new ArraryPrint(nums);
+		Object[]strs= {"안","녕","세","요"};
+		new ArraryPrint(strs);
 		String a="안녕";
 		System.out.println(a instanceof String ); //객체의 타입을 확인
 		System.out.println("안녕" instanceof String);
@@ -32,8 +44,8 @@ public class L33Instanceof {
 		Object obj_int=10;
 		Object obj_db=10.0;
 		Object obj_f=10.0f;
-
-
-		
+		Object[]obj_arr= {10,20,30,100};
+		//System.out.println(obj_int+20);
+		//모든객체의 부모인 Object에 모든 타입의 객체를 참조할 수 있지만 Object 필드만 사용가능 
 	}
 }
