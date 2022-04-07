bd77bc9ef1d828fe43207cabee2bdb2b1397638e
develckm <develckm@gmail.com>
2022-04-07 09:45:06 +0900

    random 배열 과제 풀이

--- src/com/econ/test/T06CardGame2.java
+++ src/com/econ/test/T06CardGame2.java
@@ -3,27 +3,19 @@ package com.econ.test;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Random;
 
-class Card{
-	int num;
-	String img;
-	boolean flip;
-	Card(int num){
-		this.num=num;
-	}
-}
-public class T05CardGame {
+public class T06CardGame2 {
 	public static void main(String[]args) {
 		//짝 맞추기 카드게임 
 		Integer [] cards= {1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9};
+		LinkedList<Integer> card_list=new LinkedList<Integer>(Arrays.asList(cards));
 		Integer [] random_cads=new Integer[18]; 
-		for(int i=0; i<cards.length;) {
-			int random=(int)((new Random().nextDouble())*18);			
-			if(random_cads[random]==null) {
-				random_cads[random]=cards[i];
-				i++;
-			}
+		for(int i=0; i<random_cads.length; i++) {
+			int random=(int)(new Random().nextDouble()*card_list.size());
+			int random_obj=card_list.remove(random);
+			random_cads[i]=random_obj;
 		}
 		System.out.println(new ArrayList(Arrays.asList(random_cads)));
 	}
