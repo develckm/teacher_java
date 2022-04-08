ee80a1719d791461f3be43e3dba41b805b166790
develckm <develckm@gmail.com>
2022-04-08 11:05:29 +0900

    카드게임 완성

--- src/com/econ/thread/L06CardGame.java
+++ src/com/econ/thread/L06CardGame.java
@@ -1,6 +1,9 @@
 package com.econ.thread;
 
+import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Random;
@@ -9,7 +12,8 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 class CardGameFrame extends JFrame{
 	Card[] cards=new Card[12];
-	Integer [] cards_nums= {1,1,2,2,3,3,4,4,5,5,6,6};		
+	Integer [] cards_nums= {1,1,2,2,3,3,4,4,5,5,6,6};
+	LinkedList<Card> click_cards=new LinkedList<Card>();
 	JFrame f=this;
 	public CardGameFrame(String title) throws InterruptedException {
 		super(title);
@@ -23,7 +27,7 @@ class CardGameFrame extends JFrame{
 			public void run() {
 				for(Card c :cards) {
 					try {
-						Thread.sleep(500);
+						Thread.sleep(100);
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
@@ -37,7 +41,7 @@ class CardGameFrame extends JFrame{
 			public void run() {
 				try {
 					for(Card c :cards) {
-							Thread.sleep(500);
+							Thread.sleep(100);
 						c.setText("");
 						f.validate();
 					}
@@ -46,20 +50,88 @@ class CardGameFrame extends JFrame{
 				}
 			}
 		});
-		this.setLayout(new GridLayout(3,4));
+		Thread t3=new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					Thread.sleep(500);
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
+				}
+				while(true) {
+					if(click_cards.size()==2) {
+						if(click_cards.get(0).card_num!=click_cards.get(1).card_num) {
+							try {
+								Thread.sleep(1000);
+							} catch (InterruptedException e1) {
+								e1.printStackTrace();
+							}
+
+							for(Card c: click_cards) {
+								c.setBackground(new Color(255,255,255));
+								c.setOpaque(true);
+								c.setBorderPainted(false);
+								c.setText("");
+							}
+						}
+						click_cards.remove(1);
+						click_cards.remove(0);						
+					}
+
+				}
+			}
+		});
+		this.setLayout(new GridLayout(3,4,20,10));
 		this.setBounds(600,100,600,800);
 		this.setVisible(true);
 		this.validate();//새로고침
 		t1.start();
 		t1.join();	
 		t2.start();
+		t2.join();
+		setCardAction();
+		t3.start();
 	}
 	public void randomCards() {
 		LinkedList<Integer> card_list=new LinkedList<Integer>(Arrays.asList(cards_nums));
 		for(int i=0; i< cards.length; i++){
 			int random=(int)(new Random().nextDouble()*card_list.size());
-			cards[i]=new Card(card_list.remove(random));
+			Card c=new Card(card_list.remove(random));
+			cards[i]=c;
 		}	
+	}
+	public void setCardAction() {
+		for(Card c: cards) {
+			c.addActionListener(new CardActionHander(c));
+		}
+	}
+	class CardActionHander implements ActionListener{
+		Card c;
+		CardActionHander(Card c){
+			this.c=c;
+		}
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(click_cards.size()==0) {
+				System.out.println(c.card_num);
+
+				click_cards.add(c);
+				c.setText(c.card_num+"");
+				c.setBackground(new Color(200,200,200));
+				c.setOpaque(true);
+				c.setBorderPainted(false);		
+			}else if(click_cards.size()==1) {
+				System.out.println(c.card_num);
+				click_cards.add(c);
+				c.setText(c.card_num+"");
+				c.setBackground(new Color(200,200,200));
+				c.setOpaque(true);
+				c.setBorderPainted(false);
+				c.validate();
+				f.validate();
+
+			}
+		}
 	}
 }
 class Card extends JButton{
